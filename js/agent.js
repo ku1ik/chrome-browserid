0c9ec6f31dcf1c66ce3dba6de133725ffbd0c92a
Marcin Kulik <marcin.kulik@gmail.com>
2012-12-07 00:40:11 +0100

    Use anonymous function to initialize injected agent script

--- js/agent.js
+++ js/agent.js
@@ -1,46 +1,48 @@
-// wait for polyfill to be loaded
-if (document.readyState == "complete") {
-  init();
-} else {
-  window.onload = init;
-}
-
-var loggedIn = false;
-
-function init() {
-  if (!navigator.id)
-    return;
-
-  sendToContent({ type: "icon", show: true });
-  window.addEventListener('browserid-exec', onMessage);
-  navigator.id.watch({ onlogin: onLogin, onlogout: onLogout });
-}
-
-function onLogin(assertion) {
-  console.log('onLogin');
-  loggedIn = true;
-  sendToContent({ type: 'auth', loggedIn: loggedIn });
-}
-
-function onLogout() {
-  console.log('onLogout');
-  loggedIn = false;
-  sendToContent({ type: 'auth', loggedIn: loggedIn });
-}
-
-function onMessage(event) {
-  console.log('message received in agent');
-  console.log(event.detail);
-
-  var data = event.detail;
-
-  if (data.type == 'request-login') {
-    navigator.id.request();
-  } else if (data.type == 'request-logout') {
-    navigator.id.logout();
+(function() {
+  // wait for polyfill to be loaded
+  if (document.readyState == "complete") {
+    init();
+  } else {
+    window.onload = init;
   }
-}
 
-function sendToContent(message) {
-  window.postMessage(message, "*");
-}
+  var loggedIn = false;
+
+  function init() {
+    if (!navigator.id)
+      return;
+
+    sendToContent({ type: "icon", show: true });
+    window.addEventListener('browserid-exec', onMessage);
+    navigator.id.watch({ onlogin: onLogin, onlogout: onLogout });
+  }
+
+  function onLogin(assertion) {
+    console.log('onLogin');
+    loggedIn = true;
+    sendToContent({ type: 'auth', loggedIn: loggedIn });
+  }
+
+  function onLogout() {
+    console.log('onLogout');
+    loggedIn = false;
+    sendToContent({ type: 'auth', loggedIn: loggedIn });
+  }
+
+  function onMessage(event) {
+    console.log('message received in agent');
+    console.log(event.detail);
+
+    var data = event.detail;
+
+    if (data.type == 'request-login') {
+      navigator.id.request();
+    } else if (data.type == 'request-logout') {
+      navigator.id.logout();
+    }
+  }
+
+  function sendToContent(message) {
+    window.postMessage(message, "*");
+  }
+})();
